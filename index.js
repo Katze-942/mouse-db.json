5652254a15b5af1593b6f100172c0b7d18dd105a
DarkVessel <rs5334819@gmail.com>
2021-08-09 20:30:09 +0400

    0.0.4 | Поддержка алиласов в модуле для функций.

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ try {
     fs.writeFileSync("sqlite.json", "{}");
 }
 
-module.exports = {
-    set: require("./methods/set.js"),
-};
+const file_exports = {};
+fs.readdirSync("./methods").filter(file => file.endsWith(".js")).forEach(file => {
+    file = file.slice(0, -3);
+    const fileSplit = file.split("-");
+    if (fileSplit.length == 1) file_exports[file] = require("./methods/" + file + ".js");
+    else fileSplit.forEach(f => file_exports[f] = require("./methods/" + file + ".js"));
+});
+
+module.exports = file_exports;
