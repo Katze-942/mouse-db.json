16a5f013aab78f830f398acdfe49af9328092132
DarkVessel <rs5334819@gmail.com>
2021-08-10 01:58:09 +0400

    Мелкое изменение структуры. Проверка объекта по ключу и его исправление теперь перенесено в tools.js, чтобы удобно использовать во всех функциях.

--- methods/set.js
+++ methods/set.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
-const { isObject, pathFixed } = require("../tools.js");
+const { objFixed, pathFixed } = require("../tools.js");
 const path = pathFixed();
-console.log(path);
 module.exports = (key, value) => {
     // Проверки...
     if (!key) throw TypeError("No key specified.");
@@ -12,19 +11,17 @@ module.exports = (key, value) => {
     if (JSON.stringify({ a: value }) === "{}") throw TypeError("Unsupported data type! Check what you are passing as a JSON entry.");
 
     // Вызов файла...
-    const file = require(path + "/sqlite.json");
+    let file = require(path + "/sqlite.json");
     const keySplit = key.split(".").filter(key => key != "").map(key => `["${key}"]`);
 
     // Обработка...
     if (keySplit.length === 1) file[keySplit[0].slice(2, -2)] = value;
-    else if (!isObject(eval("file" + keySplit.join("?.")))) { // Проверяем, является ли основной путь - объектом. 
-        for (let i = 0; i < keySplit.length; i++) { // Перебираем каждый ключ и если что присваиваем ему объект.
-            if (!isObject(eval("file" + keySplit.slice(0, i + 1).join("")))) eval("file" + keySplit.slice(0, i + 1).join("") + "={}");
-        };
+    else {
+        file = objFixed(file, keySplit);
         eval("file" + keySplit.join("") + "=value");
-    };
+    }
 
-    // Записываем и возвращает значение...
+    // Записываем и возвращаем значение...
     fs.writeFileSync(path + "/sqlite.json", JSON.stringify(file, null, 4));
     return value;
 }
--- tools.js
+++ tools.js
@@ -1,8 +1,17 @@
+const isObject = (obj) => Object.prototype.toString.call(obj) === "[object Object]";
 module.exports = {
-    isObject: (obj) => Object.prototype.toString.call(obj) === "[object Object]",
+    isObject,
     pathFixed: () => {
         let path = __dirname.split("/");
         if (path.includes("node_modules")) path = path.slice(0, path.indexOf("node_modules") - 1);
         return path.join("/");
+    },
+    objFixed: (obj, keySplit) => { // Фиксит объекты.
+        if (!isObject(eval("obj" + keySplit.join("?.")))) { // Проверяем, является ли основной путь - объектом. 
+            for (let i = 0; i < keySplit.length-1; i++) { // Перебираем каждый ключ и если что присваиваем ему объект.
+                if (!isObject(eval("obj" + keySplit.slice(0, i + 1).join("")))) eval("obj" + keySplit.slice(0, i + 1).join("") + "={}");
+            };
+        };
+        return obj;
     }
 }
