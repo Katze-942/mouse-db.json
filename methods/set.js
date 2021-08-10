495679a1ddeee5519c2ba8e0322fb59b9763d283
DarkVessel <rs5334819@gmail.com>
2021-08-10 14:11:54 +0400

    Мелкое изменение структуры Теперь проверки вынесены в отдельную функцию.

--- methods/set.js
+++ methods/set.js
@@ -1,15 +1,8 @@
 const fs = require("fs");
-const { objFixed, pathFixed } = require("../tools.js");
+const { objFixed, pathFixed, checkKeyValue } = require("../tools.js");
 const path = pathFixed();
-module.exports = (key, value) => {
-    // Проверки...
-    if (!key) throw TypeError("No key specified.");
-    if (typeof key != "string") throw TypeError("The key value must be a string!");
-    if (value === undefined) value = null;
-
-    // Проверка, можно ли записать такой тип данных...
-    if (JSON.stringify({ a: value }) === "{}") throw TypeError("Unsupported data type! Check what you are passing as a JSON entry.");
-
+module.exports = (key, val) => {
+    const { value } = checkKeyValue(key, val);
     // Вызов файла...
     let file = require(path + "/sqlite.json");
     const keySplit = key.split(".").filter(key => key != "").map(key => `["${key}"]`);
--- tools.js
+++ tools.js
@@ -8,10 +8,20 @@ module.exports = {
     },
     objFixed: (obj, keySplit) => { // Фиксит объекты.
         if (!isObject(eval("obj" + keySplit.join("?.")))) { // Проверяем, является ли основной путь - объектом. 
-            for (let i = 0; i < keySplit.length-1; i++) { // Перебираем каждый ключ и если что присваиваем ему объект.
+            for (let i = 0; i < keySplit.length - 1; i++) { // Перебираем каждый ключ и если что присваиваем ему объект.
                 if (!isObject(eval("obj" + keySplit.slice(0, i + 1).join("")))) eval("obj" + keySplit.slice(0, i + 1).join("") + "={}");
             };
         };
         return obj;
+    },
+    checkKeyValue: (key, value) => {
+        // Проверки...
+        if (!key) throw TypeError("No key specified.");
+        if (typeof key != "string") throw TypeError("The key value must be a string!");
+        if (value === undefined) value = null;
+
+        // Проверка, можно ли записать такой тип данных...
+        if (JSON.stringify({ a: value }) === "{}") throw TypeError("Unsupported data type! Check what you are passing as a JSON entry.");
+        return { value };
     }
 }
