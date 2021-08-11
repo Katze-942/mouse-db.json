e3e908408ae7c9552bac785611107fb82e74d77f
DarkVessel <rs5334819@gmail.com>
2021-08-11 19:40:47 +0400

    Добавление системов алиласов.

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ try {
 }
 db.json = require(db.path + "/sqlite.json");
 
+const alilases = {
+    "get": "fetch",
+    "has": "exists",
+    "remove": "subtract"
+}
+
 const file_exports = {};
 file_exports.table = db;
 const methods = ["add", "all", "push", "set", "get", "has", "delete", "remove"];
 for (let i = 0; i < methods.length; i++) {
     file_exports[methods[i]] = (key, ...args) => new db(key)[methods[i]](...args);
+    if (alilases[methods[i]]) file_exports[alilases[methods[i]]] = file_exports[methods[i]];
 }
 module.exports = file_exports;
